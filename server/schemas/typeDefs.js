944ea9b9c78f58341c5b7debd42bd3b66ce1ee3d
Rare Raptor <connorwtuncc@gmail.com>
2022-07-28 17:22:08 -0400

    got routes pulled into resolvers and models pulled into typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollop-server-express');
+
+const typeDefs = gql`
+authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    savedBooks: [bookSchema],
+  },
+`;
+
+module.exports = typeDefs;
